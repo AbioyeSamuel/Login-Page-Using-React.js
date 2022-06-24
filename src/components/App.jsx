0a12f326d27f06523a30d8db203be9b490165044
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-24 17:47:23 +0100

    log

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Login from "./Login";
 
-var isLoggedIn = false;
+var isLoggedIn =false;
 
 
 function App() {
   return (
     <div className="container">
-      isLoggedIn === true ? <h1>Hello</h1> <Login username="text" password="password" login="Login"/>
+      isLoggedIn ? <h1>Hello</h1> <Login username="text" password="password" login="Login"/>
     </div>
   );
 }
