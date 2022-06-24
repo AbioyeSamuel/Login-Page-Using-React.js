930606908cd66b30ef9f5d70c8f82f0ff13fcb91
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-24 17:55:21 +0100

    Test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,15 @@ import Login from "./Login";
 
 var isLoggedIn =false;
 
+const currentTime = new Date().getHours();
 
+console.log(currentTime);
 function App() {
   return (
     <div className="container">
-      isLoggedIn ? <h1>Hello</h1> <Login username="text" password="password" login="Login"/>
-    </div>
+      {/* isLoggedIn ? <h1>Hello</h1> <Login username="text" password="password" login="Login"/> */
+    currentTime > 12 ? <h1>Stop Working</h1> : null
+    }</div>
   );
 }
 
